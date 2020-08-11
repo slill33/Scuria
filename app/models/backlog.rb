ec4d62358179d36e0bc34f3560186fcd14b6543a
REST <rest.ngk@gmail.com>
2020-08-11 20:09:27 +0000

    refs #10 add validation

--- app/models/backlog.rb
+++ app/models/backlog.rb
@@ -4,7 +4,11 @@ class Backlog < ApplicationRecord
   has_many :children, class_name: "Backlog", foreign_key: "parent_id", dependent: :destroy
   belongs_to :parent, class_name: "Backlog", foreign_key: "parent_id", optional: true
 
+
+  before_create :ensure_super_user
   before_create :team_id_initialize
+
+  before_destroy :ensure_super_user
 
 
   def management_team
@@ -14,5 +18,12 @@ class Backlog < ApplicationRecord
   private
   def team_id_initialize
     self.team_id = self.user.team_id
+  end
+
+  def ensure_super_user
+    unless self.user.super
+      errors.add(:base, "ボードを操作する権限がありません.")
+    end
+    throw :abort if errors.messages[:base].present?
   end
 end
