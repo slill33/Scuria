011af37c8a3714cdc3c6ccd255967f1fa64056fb
REST <rest.ngk@gmail.com>
2020-08-11 16:20:36 +0000

    refs #10 validate position uniqueness for backlog_column and backlog_item

--- app/models/backlog_column.rb
+++ app/models/backlog_column.rb
@@ -1,4 +1,6 @@
 class BacklogColumn < ApplicationRecord
   belongs_to :backlog
   has_many :backlog_items
+
+  validates :position, uniqueness: true
 end
--- app/models/backlog_item.rb
+++ app/models/backlog_item.rb
@@ -1,3 +1,5 @@
 class BacklogItem < ApplicationRecord
   belongs_to :backlog_column
+
+  validates :position, uniqueness: true
 end
