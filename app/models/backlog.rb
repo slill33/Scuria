86aa57d7d56d7de3d088027e0523fb17314b6423
REST <rest.ngk@gmail.com>
2020-08-11 18:36:54 +0000

    refs #10 add validation for backlog.rb

--- app/models/backlog.rb
+++ app/models/backlog.rb
@@ -6,4 +6,13 @@ class Backlog < ApplicationRecord
   belongs_to :parent, class_name: "Backlog", foreign_key: "parent_id", optional: true
 
   #TODO backlogs <-> teams の関連付け方針
+
+  validate :team_id_check, on: :create
+
+
+  private
+  def team_id_check
+    return if self.user.team_id == self.team_id
+    errors.add(:team_id, ": Invalid team_id is set.")
+  end
 end
