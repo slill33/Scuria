493ca0312575229ade6801f30c45224cbaa96905
REST <rest.ngk@gmail.com>
2020-08-11 17:54:33 +0000

    refs #10 add self join relationship for backlog, and little fix

--- app/models/backlog.rb
+++ app/models/backlog.rb
@@ -1,6 +1,9 @@
 class Backlog < ApplicationRecord
   belongs_to :user
+  #belongs_to :team
   has_many :backlog_columns
+  has_many :children, class_name: "Backlog", foreign_key: "parent_id", dependent: :destroy
+  belongs_to :parent, class_name: "Backlog", foreign_key: "parent_id", optional: true
 
   #TODO backlogs <-> teams の関連付け方針
 end
