e7507316abfc4fadcc874a78441b616c8e45c9c7
TOMOYA SUZUKI <tomoya.suzuki.iii@gmail.com>
2020-08-13 14:09:50 +0000

    remove space

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   #has_many :backlog_items, through: :user_to_backlog_items
   belongs_to :team
   #belongs_to :backlog_members
-  
+
   acts_as_paranoid
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable,
