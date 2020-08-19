412f5f5f155931d6357ed56e326db0c248518273
REST <rest.ngk@gmail.com>
2020-08-19 21:19:26 +0000

    refs #13 add association and validation for backlog item

--- app/models/backlog_item.rb
+++ app/models/backlog_item.rb
@@ -1,8 +1,9 @@
 class BacklogItem < ApplicationRecord
   belongs_to :backlog_column
+  has_many   :children, class_name: "BacklogItem", foreign_key: "parent_id", dependent: :destroy
+  belongs_to :parent,   class_name: "BacklogItem", foreign_key: "parent_id", optional: true
 
-  validates :position, uniqueness: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :priority, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :priority, uniqueness: { scope: [:backlog_id, :backlog_column_id] }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_priority(priority)
     self.update(priority: priority)
