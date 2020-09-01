4d934dd7faf9107f142b685cfe11146b52bb7c21
REST <rest.ngk@gmail.com>
2020-09-01 17:51:00 +0000

    refs #22 fix private api

--- app/controllers/private_api/backlogs_controller.rb
+++ app/controllers/private_api/backlogs_controller.rb
@@ -1,35 +1,22 @@
+require "pry"
+
 module PrivateApi
   class BacklogsController < ApplicationController
-    COLUMN_KEYS = %i(id, name)
-    ITEM_KEYS   = %i(name)
+    COLUMN_KEYS = %i(id name color)
+    ITEM_KEYS   = %i(id name point)
+    TAG_KEYS    = %i(id name)
+    USER_KEYS   = %i(id name)
 
     skip_before_action :verify_authenticity_token
 
     before_action :parse_request_body, only: [:upsert]
     before_action :find_backlog
 
-    #def get
-    #  render json: {
-    #    status: 200,
-    #    message: params[:hashcode]
-    #  }.to_json
-    #rescue
-    #  render json: "internal server error", status: :internal_server_error
-    #end
-
-    #def upsert
-    #  render json: {
-    #    status: 200,
-    #    message: @params
-    #  }.to_json
-    #rescue
-    #  render json: "internal server error", status: :internal_server_error
-    #end
 
     def get_columns_and_items
       render json: {
         status:  200,
-        message: get_columns_and_association_items(COLUMN_KEYS, ITEM_KEYS)
+        message: get_columns_and_association_items(COLUMN_KEYS, ITEM_KEYS, TAG_KEYS, USER_KEYS)
       }.to_json
     rescue
       render json: "internal server error", status: :internal_server_error
@@ -43,9 +30,7 @@ module PrivateApi
       old_column_id      = update_target_item.column_id
       old_priority       = update_target_item.priority
 
-      backlog = Backlog.find_by(hashcode: @params[:hashcode])
-      old_column       = BacklogColumn.find_by_id(old_column_id)
-      old_column_items = old_column.backlog_items
+      old_column_items = BacklogColumn.find_by_id(old_column_id).backlog_items
       priority_max = old_column_items.maximum(:priority)
       old_column_items.where(priority: new_priority..priority_max).order(priority: "DESC")
 
@@ -59,13 +44,12 @@ module PrivateApi
         if new_priority > old_priority then
           old_column_item_ids = old_column_items.where(priority: (old_priority+1)..new_priority).pluck(:id)
           BacklogItem.decrement_counter(:priority, old_column_item_ids)
-        else if old_priority > new_priority then
+        elsif old_priority > new_priority then
           old_column_item_ids = old_column_items.where(priority: new_priority..(old_priority-1)).pluck(:id)
           BacklogItem.increment_counter(:priority, old_column_item_ids)
         end
       else
-        new_column       = BacklogColumn.find_by_id(new_column_id)
-        new_column_items = new_column.backlog_items
+        new_column_items = BacklogColumn.find_by_id(new_column_id).backlog_items
 
         old_column_item_ids = old_column_items.where(priority: (old_priority+1)..old_column_items.maximum(:priority))
         new_column_item_ids = new_column_items.where(priority: new_priority..new_column_items.maximum(:priority))
@@ -121,50 +105,68 @@ module PrivateApi
     end
 
     concerning :GetMethod do
-      def get_columns_and_association_items(column_keys, item_keys, tag_keys, assignee_keys)
-        @backlog.backlog_columns.map do |column|
+      def get_columns_and_association_items(column_keys, item_keys, tag_keys, user_keys)
+        @backlog = Backlog.find(1)
+        columns_info = @backlog.backlog_columns.map do |column|
           column_info = column_keys.map(&:intern).reduce({}) {|obj, key|
             obj.tap {|me| me[key] = column[key] }
           }
-          column_info[:items] = get_items_by_column(column, item_keys)
+          column_info[:items] = get_items_by_column(column, item_keys, tag_keys, user_keys)
+
+          column_info
         end
+
+        return {columns: columns_info}
       end
 
-      def get_items_by_column(column, keys, tag_keys, assignee_keys)
-        column.backlog_items.map do |item|
+      def get_items_by_column(column, keys, tag_keys, user_keys)
+        items_info = column.backlog_items.map do |item|
           item_info = keys.map(&:intern).reduce({}) {|obj, key|
             obj.tap {|me| me[key] = item[key]}
           }
-          item_info[:tags]      = get_tags(item, tag_keys)
-          item_info[:assignees] = get_assignees(item, assignee_keys)
+          item_info[:tags]  = get_tags(item, tag_keys)
+          item_info[:users] = get_users(item, user_keys)
+
+          item_info
         end
+
+        return items_info
       end
 
       def get_tags(item, keys)
-        item.backlog_tags.map do |tag|
-          keys.map(&:intern).reduce({}) {|obj, key|
-            obj.tap {|me| me[key] = item[key]}
-          }
-        end
+        # TODO: uncomment out 
+        #normalize_update_infos(item.backlog_tags, keys)
+        return []
       end
 
-      def get_assignees(item, keys)
-        item.users.map do |tag|
-          keys.map(&:intern).reduce({}) {|obj, key|
-            obj.tap {|me| me[key] = item[key]}
-          }
-        end
+      def get_users(item, keys)
+        # TODO: uncomment out 
+        #normalize_update_infos(item.users, keys)
+        return []
       end
     end
 
     concerning :UpdateMethod do
-      def update_backlog_items_priority(old_column_id, new_column_id, priority)
-      end
+      #def update_backlog_items_priority(old_column_id, new_column_id, priority)
+      #end
     end
 
     def backlog_item_params
-      # strong parameter: あってるか自信ない
       @paramas.require(:backlog_item).permit(:priority, :backlog_column_id)
+    end
+
+    def normalize_update_infos(keys, update_infos, *subordinate_infos)
+      infos = update_infos.map {|update_info|
+        keys.map(&:intern).reduce({}) {|obj, key|
+          obj.tap {|me| me[key] = update_info[key]}
+        }
+      }
+
+      subordinate_infos.keys {|key|
+        infos[key] = subordinate_infos[key]
+      } unless subordinate_infos.empty?
+
+      return infos
     end
 
   end
