1c60bbc7610073b29f3ade94a7fe3bdbc1a9a821
TOMOYA SUZUKI <tomoya.suzuki.iii@gmail.com>
2020-09-02 18:24:55 +0000

    refs 15 fix

---
+++ app/javascript/Backlog.vue
@@ -0,0 +1,207 @@
+<template>
+  <div id="backlog">
+    <div class="example-modal-window">
+      <p>ボタンを押すとモーダルウィンドウが開きます</p>
+      <button @click="openModal">開く</button>
+
+      <!-- コンポーネント MyModal -->
+      <MyModal @close="closeModal" v-if="modal">
+        <!-- default スロットコンテンツ -->
+        <p>Vue.js Modal Window!</p>
+        <div>
+          <input v-model="message" />
+        </div>
+        <!-- /default -->
+        <!-- footer スロットコンテンツ -->
+        <template slot="footer">
+          <button @click="doSend">送信</button>
+        </template>
+        <!-- /footer -->
+      </MyModal>
+    </div>
+    <div class="columns">
+      <div
+        v-for="(column, index) in columns"
+        :key="index"
+        class="card text-white mb-3 column"
+        v-bind:style="{ 'background-color': column.color}"
+      >
+        <div class="card-header column-header">
+          <div class="column-name">{{column.name}}</div>
+          <i class="material-icons md-24 column-header-icon">settings</i>
+        </div>
+        <draggable
+          v-model="columns[index].items"
+          tag="div"
+          class="card-body items"
+          group="items"
+          @end="itemDraggableEnd"
+          :data-column-id="column.columnId"
+        >
+          <Item v-for="item in column.items" :key="item.itemId" :item="item"></Item>
+        </draggable>
+        <div class="card-footer">
+          <div class="item-name" style="text-align:right">
+            <i class="material-icons md-24">add</i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+import Item from "./Item.vue";
+import MyModal from "./ItemModal.vue";
+
+export default {
+  components: {
+    MyModal,
+    draggable,
+    Item
+  },
+  data() {
+    return {
+      modal: true,
+      message:'',
+      backlogId: this.$route.params["id"],
+      columns: [
+        {
+          name: "Todo",
+          columnId: 1,
+          color: "#28a745",
+          items: [
+            {
+              itemId: 1,
+              name: "task1-1",
+              tags: [
+                { name: "aaaa", tagId: 1 },
+                { name: "iii", tagId: 2 }
+              ],
+              users: [
+                { name: "yamda", userId: 1 },
+                { name: "satou", userId: 2 }
+              ],
+              point: 0
+            },
+            { itemId: 2, name: "task1-3", tags: [], users: [], point: 10 },
+            { itemId: 3, name: "task1-2", tags: [], users: [], point: 100 }
+          ]
+        },
+        {
+          name: "Doing",
+          columnId: 2,
+          color: "#6f42c1",
+          items: [
+            {
+              itemId: 4,
+              name: "task1-1",
+              tags: [
+                { name: "aaaa", tagId: 1 },
+                { name: "iii", tagId: 2 }
+              ],
+              users: [
+                { name: "yamda", userId: 1 },
+                { name: "satou", userId: 2 }
+              ],
+              point: 0
+            },
+            { itemId: 5, name: "task1-3", tags: [], users: [], point: 10 },
+            { itemId: 6, name: "task1-2", tags: [], users: [], point: 100 }
+          ]
+        },
+        {
+          name: "Done",
+          columnId: 3,
+          color: "#007bff",
+          items: [
+            {
+              itemId: 7,
+              name: "task1-1",
+              tags: [
+                { name: "aaaa", tagId: 1 },
+                { name: "iii", tagId: 2 }
+              ],
+              users: [
+                { name: "yamda", userId: 1 },
+                { name: "satou", userId: 2 }
+              ],
+              point: 0
+            },
+            { itemId: 8, name: "task1-3", tags: [], users: [], point: 10 },
+            { itemId: 9, name: "task1-2", tags: [], users: [], point: 100 }
+          ]
+        }
+        /*         {
+          name: "doing",
+          columnId: 200,
+          items: [{ name: "task2-1" }, { name: "task2-3" }, { name: "task2-2" }]
+        },
+        {
+          name: "done",
+          columnId: 300,
+          items: [{ name: "task3-1" }, { name: "task3-3" }, { name: "task3-2" }]
+        } */
+      ],
+      tasks: [
+        ["task1-1", "task1-2", "task1-3", "taskx"],
+        ["task2-1", "task2-2", "task2-3", "task2-4"],
+        ["task3-1", "task3-2", "task3-3"]
+      ]
+    };
+  },
+  methods: {
+    itemDraggableEnd: function(event) {
+      var test = event;
+      console.log(event);
+      console.log(event.from.dataset.columnId);
+      console.log(event.to.dataset.columnId);
+    },
+    openModal() {
+      this.modal = true
+    },
+    closeModal() {
+      this.modal = false
+    },
+    doSend() {
+      if (this.message.length > 0) {
+        alert(this.message)
+        this.message = ''
+        this.closeModal()
+      } else {
+        alert('メッセージを入力してください')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+/* ---------------------------column用---------------------------*/
+#backlog {
+  margin: 1rem;
+}
+
+.columns {
+  display: flex;
+  align-items: flex-start;
+}
+.column {
+  margin: 0rem 0.3rem;
+  width: 20rem;
+}
+.column-header {
+  display: flex;
+}
+.column-name {
+  float: left;
+}
+.column-header-icon {
+  margin-left: auto;
+}
+
+.items .item:first-child {
+  margin: 0rem 0rem 0.4rem 0rem;
+}
+</style>
