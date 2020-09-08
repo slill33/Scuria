a8626efdb09a0a0364747fbe61f7ce300ec24e23
TOMOYA SUZUKI <tomoya.suzuki.iii@gmail.com>
2020-09-08 03:40:39 +0000

    refs #29 modal

--- app/javascript/Backlog.vue
+++ app/javascript/Backlog.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="backlog">
+    <div class="example-modal-window">
+      <p>ボタンを押すとモーダルウィンドウが開きます</p>
+      <button @click="openModal">開く</button>
+
+
+      <!-- コンポーネント ItemModal -->
+      <ItemModal @close="closeModal" v-if="modal">
+        <!-- default スロットコンテンツ -->
+        <p>Vue.js Modal Window!</p>
+        <div>
+          <input v-model="message" />
+        </div>
+        <!-- /default -->
+        <!-- footer スロットコンテンツ -->
+        <template slot="footer">
+          <button @click="doSend">送信</button>
+        </template>
+        <!-- /footer -->
+      </ItemModal>
+    </div>
     <div class="columns">
       <div
         v-for="(column, index) in columns"
@@ -28,33 +48,37 @@
         </div>
       </div>
     </div>
-  <span>{{$data}}</span>
+    <span>{{$data}}</span>
   </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
 import Item from "./Item.vue";
-import axios from 'axios';
+import ItemModal from "./ItemModal.vue";
+import axios from "axios";
 export default {
   components: {
+    ItemModal,
     draggable,
     Item
   },
   data() {
     return {
+      modal: true,
       message:'',
-      backlogId: this.$route.params["id"],
-      columns:[]
+      columns: []
     };
   },
-  mounted (){
+  mounted() {
     axios
-    .get(`/api/v1/private/backlogs/${this.$route.params['id']}/columns_and_items_get.json`)
-    .then(response => (this.columns = response.data.message.columns))
-    .catch(error => {
-      alert("更新できませんでした")
-    })
+      .get(
+        `/api/v1/private/backlogs/${this.$route.params["id"]}/columns_and_items_get.json`
+      )
+      .then(response => (this.columns = response.data.message.columns))
+      .catch(error => {
+        alert("更新できませんでした");
+      });
   },
   methods: {
     itemDraggableEnd: function(event) {
@@ -63,8 +87,23 @@ export default {
       console.log(event.from.dataset.columnId);
       console.log(event.to.dataset.columnId);
     },
+    openModal() {
+      this.modal = true
+    },
+    closeModal() {
+      this.modal = false
+    },
+    doSend() {
+      if (this.message.length > 0) {
+        alert(this.message)
+        this.message = ''
+        this.closeModal()
+      } else {
+        alert('メッセージを入力してください')
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
