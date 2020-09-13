4592801043322fe96c0b85b305a0eae5cabd4c39
TOMOYA SUZUKI <tomoya.suzuki.iii@gmail.com>
2020-09-13 17:09:22 +0000

    refs #29 add

--- app/javascript/Backlog.vue
+++ app/javascript/Backlog.vue
@@ -3,28 +3,13 @@
     <div class="example-modal-window">
       <p>ボタンを押すとモーダルウィンドウが開きます</p>
       <button @click="openModal">開く</button>
-
-
-      <!-- コンポーネント ItemModal -->
-      <ItemModal @close="closeModal" v-if="modal">
-        <!-- default スロットコンテンツ -->
-        <p>Vue.js Modal Window!</p>
-        <div>
-          <input v-model="message" />
-        </div>
-        <!-- /default -->
-        <!-- footer スロットコンテンツ -->
-        <template slot="footer">
-          <button @click="doSend">送信</button>
-        </template>
-        <!-- /footer -->
-      </ItemModal>
+      <ItemModal @close="closeModal" v-if="modal" :item="modalItem" :users="users" :tags="tags"></ItemModal>
     </div>
-    <div class="columns">
+    <draggable v-model="columns" class="columns" group="columns">
       <div
         v-for="(column, index) in columns"
         :key="index"
-        class="card text-white mb-3 column"
+        class="card text-white column"
         v-bind:style="{ 'background-color': column.color}"
       >
         <div class="card-header column-header">
@@ -33,13 +18,19 @@
         </div>
         <draggable
           v-model="columns[index].items"
-          tag="div"
           class="card-body items"
           group="items"
           @end="itemDraggableEnd"
           :data-column-id="column.id"
         >
-          <Item v-for="item in column.items" :key="item.id" :item="item"></Item>
+          <Item
+            v-for="item in column.items"
+            :key="item.id"
+            :item="item"
+            @edit="editItem"
+            :users="users"
+            :tags="tags"
+          ></Item>
         </draggable>
         <div class="card-footer">
           <div class="item-name" style="text-align:right">
@@ -47,7 +38,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </draggable>
     <span>{{$data}}</span>
   </div>
 </template>
@@ -65,42 +56,113 @@ export default {
   },
   data() {
     return {
-      modal: true,
-      message:'',
-      columns: []
+      modal: false,
+      message: "",
+      columns: [],
+      modalItem: {},
+      users: {
+        1: { name: "UserA" },
+        2: { name: "UserB" },
+        3: { name: "UserC" },
+        4: { name: "UserD" }
+      },
+      tags: {
+        1: { name: "tagA" },
+        2: { name: "tagB" },
+        3: { name: "tagC" },
+        4: { name: "tagD" }
+      }
     };
   },
   mounted() {
-    axios
-      .get(
-        `/api/v1/private/backlogs/${this.$route.params["id"]}/columns_and_items_get.json`
-      )
-      .then(response => (this.columns = response.data.message.columns))
-      .catch(error => {
-        alert("更新できませんでした");
-      });
+    // axios
+    //   .get(
+    //     `/api/v1/private/backlogs/${this.$route.params["id"]}/columns_and_items_get.json`
+    //   )
+    //   .then(response => (this.columns = response.data.message.columns))
+    //   .catch(error => {
+    //     alert("更新できませんでした");
+    //   });
+    this.columnSample();
   },
   methods: {
     itemDraggableEnd: function(event) {
       var test = event;
       console.log(event);
-      console.log(event.from.dataset.columnId);
-      console.log(event.to.dataset.columnId);
+      console.log(event.from.dataset.id);
+      console.log(event.to.dataset.id);
     },
     openModal() {
-      this.modal = true
+      this.modal = true;
     },
     closeModal() {
-      this.modal = false
+      this.modal = false;
     },
-    doSend() {
-      if (this.message.length > 0) {
-        alert(this.message)
-        this.message = ''
-        this.closeModal()
-      } else {
-        alert('メッセージを入力してください')
-      }
+    editItem(item) {
+      this.openModal();
+      this.modalItem = item;
+    },
+    columnSample() {
+      this.columns = [
+        {
+          name: "Todo",
+          id: 1,
+          color: "#28a745",
+          items: [
+            {
+              id: 1,
+              name: "task1-1",
+              tags: [1, 2],
+              users: [1, 2],
+              point: 1
+            },
+            { id: 2, name: "task1-3", tags: [], users: [], point: 1 },
+            { id: 3, name: "task1-2", tags: [], users: [], point: 100 }
+          ]
+        },
+        {
+          name: "Doing",
+          id: 2,
+          color: "#6f42c1",
+          items: [
+            {
+              id: 4,
+              name: "task1-1",
+              tags: [3, 4],
+              users: [3, 4],
+              point: 0
+            },
+            { id: 5, name: "task1-3", tags: [], users: [], point: 10 },
+            { id: 6, name: "task1-2", tags: [], users: [], point: 100 }
+          ]
+        },
+        {
+          name: "Done",
+          id: 3,
+          color: "#007bff",
+          items: [
+            {
+              id: 7,
+              name: "task1-1",
+              tags: [3, 4],
+              users: [3, 4],
+              point: 0
+            },
+            { id: 8, name: "task1-3", tags: [], users: [], point: 10 },
+            { id: 9, name: "task1-2", tags: [], users: [], point: 100 }
+          ]
+        }
+        /*         {
+          name: "doing",
+          id: 200,
+          items: [{ name: "task2-1" }, { name: "task2-3" }, { name: "task2-2" }]
+        },
+        {
+          name: "done",
+          id: 300,
+          items: [{ name: "task3-1" }, { name: "task3-3" }, { name: "task3-2" }]
+        } */
+      ];
     }
   }
 };
--- app/javascript/ItemModal.vue
+++ app/javascript/ItemModal.vue
@@ -2,20 +2,105 @@
   <transition name="modal" appear>
     <div class="modal modal-overlay" @click.self="$emit('close')">
       <div class="modal-window">
-        <div class="modal-content">
+        <!--         <div class="modal-content">
           <slot/>
         </div>
         <footer class="modal-footer">
           <slot name="footer">
-            <!-- <button @click="$emit('close')">Close</button> -->
+            <button @click="$emit('close')">Close</button>
             <button >Close</button>
           </slot>
-        </footer>
+        </footer>-->
+        <div class="modal-content">
+          <!-- 入力フォームを２列で表示 -->
+          <input
+            type="name"
+            class="form-control mb-4"
+            placeholder="ItemName"
+            style="max-width:30rem"
+            v-model="item.name"
+          />
+          <div class="modal-content-2rows">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="mail2">説明</label>
+                <textarea class="form-control" placeholder="Description" style="height:6.5rem"></textarea>
+              </div>
+              <div class="col-md-6">
+                <div class="modal-right-content">
+                  <label for="Point">Point</label>
+                  <select class="form-control" placeholder="Point" v-model="item.point">
+                    <option :value="0">選択してください</option>
+                    <option :value="n" v-for="n of 89" :key="n">{{n}}</option>
+                  </select>
+                </div>
+                <div class="modal-right-content">
+                  <div class="modal-right-content-label-box">
+                    <p>作業者</p>
+                    <i class="material-icons md-24">add</i>
+                  </div>
+                  <div v-if="item.users.length>0" class="item-tags">
+                    <!-- <span v-for="tagId in item.tags" :key="tagId" class="tag">{{tags[tagId].name}}</span> -->
+                    <div
+                      v-for="(userId, index) in item.users"
+                      class="modal-right-content-form-box"
+                      :key="userId + '_' +index"
+                    >
+                        <select class="form-control" placeholder="作業者" v-model="item.users[index]">
+                          <option
+                            v-for="(value, key) in users"
+                            :key="value.name"
+                            :value="key"
+                          >{{value.name}}</option>
+                        </select>
+                      <i class="material-icons md-24 column-header-icon">clear</i>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="modal-right-content">
+                  <div class="modal-right-content-label-box">
+                    <p>タグ</p>
+                  </div>
+                  <div class="item-tags">
+                    <span class="modal-tag">a</span>
+                    <span class="modal-tag">b</span>
+                    <span class="modal-tag">c</span>
+                    <span class="modal-tag">c</span>
+                    <span class="modal-tag">c</span>
+                    <span class="modal-tag setting-modal-tag">caaaaa</span>
+                    <span class="modal-tag setting-modal-tag">caaaaa</span>
+                    <i class="material-icons md-24">add</i>
+                  </div>
+                  <div class="modal-right-content-form-box">
+                    <input type="text" id="text1" class="form-control" />
+                    <i class="material-icons md-24 column-header-icon">check</i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </transition>
 </template>
-
+<script>
+export default {
+  props: ["item", "users", "tags"],
+  methods: {
+    openModal() {
+      console.log(this.item);
+    }
+  },
+  data() {
+    return {
+      itemUsers: ([] = this.item.users)
+    };
+  },
+  watch: {}
+};
+</script>
 <style lang="scss" scoped>
 .modal {
   &.modal-overlay {
@@ -34,9 +119,10 @@
     background: #fff;
     border-radius: 4px;
     overflow: hidden;
+    width: 30rem;
   }
   &-content {
-    padding: 10px 20px;
+    padding: 1.5rem;
   }
   &-footer {
     background: #ccc;
@@ -44,7 +130,8 @@
     text-align: right;
   }
 }
-.modal-enter-active, .modal-leave-active {
+.modal-enter-active,
+.modal-leave-active {
   transition: opacity 0.4s;
   .modal-window {
     transition: opacity 0.4s, transform 0.4s;
@@ -53,11 +140,66 @@
 .modal-leave-active {
   transition: opacity 0.6s ease 0.4s;
 }
-.modal-enter, .modal-leave-to {
+.modal-enter,
+.modal-leave-to {
   opacity: 0;
   .modal-window {
     opacity: 0;
     transform: translateY(-20px);
   }
+}
+
+/* ---------------------------modal---------------------------*/
+
+.modal-right-content {
+  margin: 0.5rem 0;
+}
+.modal-right-content-label-box {
+  display: flex;
+  margin: 0.2rem;
+}
+.modal-right-content-label-box > p {
+  margin: 0;
+}
+.modal-right-content-form-box {
+  display: flex;
+}
+.modal-right-content-form-box > i {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+
+.modal-assignee {
+  float: left;
+}
+.modal-tag {
+  display: inline-block;
+  height: 1.7rem;
+  padding: 0 0.3rem;
+  background-color: #fff;
+  border: 0.01rem solid #aaa;
+  border-radius: 0.1rem;
+  white-space: nowrap;
+  text-overflow: ellipsisu;
+  overflow: hidden;
+  color: #333;
+  font-size: 1em;
+  text-decoration: none;
+  -webkit-transition: 0.2s;
+  transition: 0.2s;
+}
+.modal-tag:hover {
+  background-color: #3498db;
+  border: 1px solid #3498db;
+  color: #fff;
+}
+.setting-modal-tag {
+  background-color: #3498db;
+  border: 1px solid #3498db;
+  color: #fff;
+}
+
+.modal-content-2rows {
+  margin-bottom: 0;
 }
 </style> 
