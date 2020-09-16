95ca3adef03bb307ccb8f0109915be01e5f3de44
REST <rest.ngk@gmail.com>
2020-09-16 19:31:47 +0000

    refs #25 add backlog_item_comments table

--- app/models/backlog_item.rb
+++ app/models/backlog_item.rb
@@ -2,6 +2,7 @@ class BacklogItem < ApplicationRecord
   belongs_to :backlog_column
   has_many   :children, class_name: "BacklogItem", foreign_key: "parent_id", dependent: :destroy
   belongs_to :parent,   class_name: "BacklogItem", foreign_key: "parent_id", optional: true
+  has_many   :backlog_item_comments
 
   validates :priority, uniqueness: { scope: [:backlog_id, :backlog_column_id] }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
