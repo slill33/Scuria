900a164476c7fd72a58ed4c18fcd286c1a981011
TOMOYA SUZUKI <tomoya.suzuki.iii@gmail.com>
2020-09-16 15:02:05 +0000

    refs #30 remove comments

--- app/javascript/packs/store/index.js
+++ app/javascript/packs/store/index.js
@@ -3,20 +3,7 @@ import Vuex from "vuex";
 import { mutations } from "./mutations";
 import actions from "./actions";
 Vue.use(Vuex);
-// export const store = new Vuex.Store({
-//   state: {
-//     items: [
-//       { is_do: false, title: "タスク1" },
-//       { is_do: true, title: "タスク2" },
-//       { is_do: false, title: "タスク3" }
-//     ]
-//     // backlogId: this.$route.params["id"]
-//   },
-//   mutations,
-//   actions
-// });
 const state = {
-  name: "name",
   columns:[]
 };
 
