d4ed19e63f4625d597adf873aabe37ffedaf9ba0
REST <rest.ngk@gmail.com>
2020-09-27 21:41:38 +0000

    refs #42 add the function to create default backlog_type when creating team.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,16 @@ class Team < ApplicationRecord
   has_many :users
   has_many :backlogs
   has_many :backlog_types
+
+  after_create :create_default_backlog_type
+
+
+  def create_default_backlog_type
+    return unless self.backlog_types.empty?
+
+    BacklogType::DEFAULT_NAMES.each {|default_type_name|
+      self.backlog_types.create!(name: default_type_name)
+    }
+  end
+
 end
