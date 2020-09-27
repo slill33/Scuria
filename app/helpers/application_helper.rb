b5f0666893744df223537d6015ba3ab10527728c
TOMOYA SUZUKI <tomoya.suzuki.iii@gmail.com>
2020-09-27 13:03:14 +0000

    refs #38 fix_header_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def current_backlogs
+    return Backlog.where(team_id:current_user.team_id)
+  end
+  def current_backlog_name
+    if params[:controller] == "backlogs"
+      return Backlog.find_by_id(params[:id]).name
+    else
+      return current_backlogs.first.name
+    end
+  end
 end
