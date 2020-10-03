09b4266871fd542a85bb8e6af0ea50ad14fc7b6c
Tomoya Suzuki <tomoya.suzuki.iii@gmail.com>
2020-10-03 18:51:25 +0000

    refs #21 create_index

---
+++ app/assets/javascripts/backlog.js
@@ -0,0 +1,16 @@
+;
+(function () {
+    var root = this;
+    root.backlogIndexFunc = function () {
+        window: onload = function () {
+            $('#display-affiliate').click(function () {
+                $('#affiliate-backlogs').show();
+                $('#all-backlogs').hide();
+            })
+            $('#display-all').click(function () {
+                $('#all-backlogs').show();
+                $('#affiliate-backlogs').hide();
+            })
+        };
+    }
+}).call(this);;
