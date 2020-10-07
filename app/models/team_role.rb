4aa01ea16ac657f75c9a4a7586f81761bcbfe3bd
Tomoya Suzuki <tomoya.suzuki.iii@gmail.com>
2020-10-07 01:03:51 +0000

    refs #46 remove_comment

--- app/models/team_role.rb
+++ app/models/team_role.rb
@@ -2,6 +2,5 @@ class TeamRole < ApplicationRecord
   DEFAULT_NAMES = %w(ScrumMaster ProductOwner Member)
   belongs_to :team
   has_many :user_to_backlogs, dependent: :destroy
-  #has_many :users, through: :user_to_backlogs
   validates :name, presence: true, uniqueness: { scope: :team_id }, length: { maximum: 255 }
 end
