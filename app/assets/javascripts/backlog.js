2c01303e3863ffa3cdfd5ea34181f023e76beb42
Tomoya Suzuki <tomoya.suzuki.iii@gmail.com>
2020-10-07 01:02:10 +0000

    refs 45 fix_id

--- app/assets/javascripts/backlog.js
+++ app/assets/javascripts/backlog.js
@@ -16,33 +16,33 @@
         window: onload = function () {
             $("#user-add").click(function () {
                 $(".tmp > .user-setting").clone(true).appendTo("#users-box")
-                let userIds = JSON.parse($("#user_ids").val());
+                let userIds = JSON.parse($("#user_and_role_ids").val());
                 userIds.push({ user_id: 0, role_id: 0 })
-                $("#user_ids").val(JSON.stringify(userIds))
+                $("#user_and_role_ids").val(JSON.stringify(userIds))
             });
             $(".user-remove").click(function () {
                 const thisBox = $(this).parents('.user-setting')
                 const i = thisBox.index();
-                let userIds = JSON.parse($("#user_ids").val());
+                let userIds = JSON.parse($("#user_and_role_ids").val());
                 userIds.splice(i, 1)
-                $("#user_ids").val(JSON.stringify(userIds))
+                $("#user_and_role_ids").val(JSON.stringify(userIds))
                 thisBox.remove();
 
             });
             $("#select_role").change(function () {
                 const i = $(this).parents('.user-setting').index();
-                let userIds = JSON.parse($("#user_ids").val());
+                let userIds = JSON.parse($("#user_and_role_ids").val());
                 userIds[i]["role_id"] = $(this).val()
-                $("#user_ids").val(JSON.stringify(userIds))
+                $("#user_and_role_ids").val(JSON.stringify(userIds))
             });
             $("#select_user").change(function () {
                 const i = $(this).parents('.user-setting').index();
-                let userIds = JSON.parse($("#user_ids").val());
+                let userIds = JSON.parse($("#user_and_role_ids").val());
                 userIds[i]["user_id"] = $(this).val()
-                $("#user_ids").val(JSON.stringify(userIds))
+                $("#user_and_role_ids").val(JSON.stringify(userIds))
             });
             $('#submit-btn').on('click', function (e) {
-                const userIds = JSON.parse($("#user_ids").val());
+                const userIds = JSON.parse($("#user_and_role_ids").val());
                 var checkIds = []
                 for (let i = 0; i < userIds.length; i++) {
                     if (userIds[i]["user_id"] == 0 || userIds[i]["role_id"] == 0 || checkIds.indexOf(userIds[i]["user_id"]) != -1) {
