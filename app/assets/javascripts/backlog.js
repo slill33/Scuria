cbd8e46316744427f3bef38edcbfad633e6be335
Tomoya Suzuki <tomoya.suzuki.iii@gmail.com>
2020-10-07 06:28:59 +0000

    refs #47 edit_backlog

--- app/assets/javascripts/backlog.js
+++ app/assets/javascripts/backlog.js
@@ -2,59 +2,66 @@
     let root = this;
     root.backlogIndexFunc = function () {
         window: onload = function () {
-            $("#display-affiliate").click(function () {
-                $("#affiliate-backlogs").show();
-                $("#all-backlogs").hide();
+            $('#display-affiliate').click(function () {
+                $('#affiliate-backlogs').show();
+                $('#all-backlogs').hide();
             });
-            $("#display-all").click(function () {
-                $("#all-backlogs").show();
-                $("#affiliate-backlogs").hide();
+            $('#display-all').click(function () {
+                $('#all-backlogs').show();
+                $('#affiliate-backlogs').hide();
             });
         };
     };
-    root.backlogNewFunc = function () {
+    root.backlogFormFunc = function () {
         window: onload = function () {
-            $("#user-add").click(function () {
-                $(".tmp > .user-setting").clone(true).appendTo("#users-box")
-                let userIds = JSON.parse($("#user_and_role_ids").val());
-                userIds.push({ user_id: 0, role_id: 0 })
-                $("#user_and_role_ids").val(JSON.stringify(userIds))
+            $('#user-add').click(function () {
+                $('.tmp > .user-setting').clone(true).appendTo('#users-box')
+                let userAndRoleIds = JSON.parse($('#user_and_role_ids').val());
+                userAndRoleIds.push({ user_id: 0, role_id: 0 })
+                $('#user_and_role_ids').val(JSON.stringify(userAndRoleIds))
             });
-            $(".user-remove").click(function () {
+            $('.user-remove').click(function () {
                 const thisBox = $(this).parents('.user-setting')
                 const i = thisBox.index();
-                let userIds = JSON.parse($("#user_and_role_ids").val());
-                userIds.splice(i, 1)
-                $("#user_and_role_ids").val(JSON.stringify(userIds))
+                let userAndRoleIds = JSON.parse($('#user_and_role_ids').val());
+                userAndRoleIds.splice(i, 1)
+                $('#user_and_role_ids').val(JSON.stringify(userAndRoleIds))
                 thisBox.remove();
 
             });
-            $("#select_role").change(function () {
+            $('#select_role').change(function () {
                 const i = $(this).parents('.user-setting').index();
-                let userIds = JSON.parse($("#user_and_role_ids").val());
-                userIds[i]["role_id"] = $(this).val()
-                $("#user_and_role_ids").val(JSON.stringify(userIds))
+                let userAndRoleIds = JSON.parse($('#user_and_role_ids').val());
+                userAndRoleIds[i]['role_id'] = $(this).val()
+                $('#user_and_role_ids').val(JSON.stringify(userAndRoleIds))
             });
-            $("#select_user").change(function () {
+            $('#select_user').change(function () {
                 const i = $(this).parents('.user-setting').index();
-                let userIds = JSON.parse($("#user_and_role_ids").val());
-                userIds[i]["user_id"] = $(this).val()
-                $("#user_and_role_ids").val(JSON.stringify(userIds))
+                let userAndRoleIds = JSON.parse($('#user_and_role_ids').val());
+                userAndRoleIds[i]['user_id'] = $(this).val()
+                $('#user_and_role_ids').val(JSON.stringify(userAndRoleIds))
             });
             $('#submit-btn').on('click', function (e) {
-                const userIds = JSON.parse($("#user_and_role_ids").val());
+                const userAndRoleIds = JSON.parse($('#user_and_role_ids').val());
                 var checkIds = []
-                for (let i = 0; i < userIds.length; i++) {
-                    if (userIds[i]["user_id"] == 0 || userIds[i]["role_id"] == 0 || checkIds.indexOf(userIds[i]["user_id"]) != -1) {
-                        alert("参加ユーザー設定が正しくありません")
+                for (let i = 0; i < userAndRoleIds.length; i++) {
+                    if (userAndRoleIds[i]['user_id'] == 0 || userAndRoleIds[i]['role_id'] == 0 || checkIds.indexOf(userAndRoleIds[i]['user_id']) != -1) {
+                        alert('参加ユーザー設定が正しくありません')
                         return false;
                     }
-                    checkIds.push(userIds[i]["user_id"])
+                    checkIds.push(userAndRoleIds[i]['user_id'])
                 }
             });
-            // $(".select-user").click(function () {
-            //     let i = $(this).parents('.user-setting').index();
-            // });
+            restore_user_and_role_ids()
+            function restore_user_and_role_ids() {
+                const userAndRoleIds = JSON.parse($('#user_and_role_ids').val());
+                for (let i = 0; i < userAndRoleIds.length; i++) {
+                    thisBox = $('.tmp > .user-setting').clone(true).appendTo('#users-box')
+                    console.log(userAndRoleIds[i]['user_id'])
+                    $(thisBox).find('#select_user').val(userAndRoleIds[i]['user_id'])
+                    $(thisBox).find('#select_role').val(userAndRoleIds[i]['role_id'])
+                }
+            }
         };
     };
 }.call(this));
