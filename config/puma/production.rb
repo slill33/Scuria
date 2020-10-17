d77e406a2c8b0ee84e0aac6fce69eb8ee3210560
REST <rest.ngk@gmail.com>
2020-10-17 13:09:01 +0000

    refs #60 fix puma setting on production

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,8 +1,22 @@
-environment 'production'
-bind 'tcp://127.0.0.1:3001'
-workers 2
-threads 0, 2
-daemonize true
-pidfile "#{Dir.pwd}/tmp/pids/puma.pid"
-state_path "#{Dir.pwd}/tmp/pids/puma.state"
+#!/usr/bin/env puma
+
 directory '/var/www/applications/scuria/current'
+environment 'production'
+
+tag ''
+
+pidfile "/var/www/applications/scuria/shared/tmp/pids/puma.pid"
+state_path "/var/www/applications/scuria/shared/tmp/pids/puma.state"
+stdout_redirect '/var/www/applications/scuria/shared/log/puma_access.log', '/var/www/applications/scuria/shared/log/puma_error.log', true
+
+bind 'tcp://127.0.0.1:3001'
+
+threads 0, 2
+workers 2
+
+
+prune_bundler
+
+on_restart do
+  puts 'On restart'
+end
